5dd323bfb0793208ec1dafe33437696093e81303
wcepress4 <wcepress@yahoo.com>
2024-06-14 11:57:27 -0400

    tailwind setup correctly

--- pump-pro/src/components/HeaderComponent.js
+++ pump-pro/src/components/HeaderComponent.js
@@ -4,32 +4,31 @@ import { VscAccount } from 'react-icons/vsc';
 
 const HeaderComponent = () => {
   return (
-    <div>
-      <header>
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-          <Link to="/" className="navbar-brand d-flex align-items-center">
-            PumpPro
-          </Link>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to="/profile" className="nav-link">
-                <VscAccount style={{ marginRight: '5px' }} />
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/history" className="nav-link">
-                History
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/exercises" className="nav-link">
-                Exercises
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header className="bg-white shadow-md">
+      <nav className="container mx-auto flex items-center justify-between p-4">
+        <Link to="/" className="flex items-center text-xl font-bold text-red-600">
+          <VscAccount className="mr-2 text-2xl" />
+          PumpPro
+        </Link>
+        <ul className="flex space-x-6">
+          <li>
+            <Link to="/profile" className="text-gray-700 hover:text-red-600">
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link to="/history" className="text-gray-700 hover:text-red-600">
+              History
+            </Link>
+          </li>
+          <li>
+            <Link to="/exercises" className="text-gray-700 hover:text-red-600">
+              Exercises
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
